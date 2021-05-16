05d4657240547c5f254818a1832d541bd33cedaf
milindsingh <milind7@live.com>
2021-05-16 18:51:07 +0530

    init commit

---
+++ preview/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"net/url"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/badoux/goscraper"
+)
+
+type Preview struct {
+    Url   	  string   `json:"url"`
+    Title         string   `json:"title"`
+    SiteName      string   `json:"siteName"`
+    Description   string   `json:"description"`
+    Images      []string   `json:"images"`
+    Image         string   `json:"image"`
+}
+
+// Response is of type APIGatewayProxyResponse since we're leveraging the
+// AWS Lambda Proxy Request functionality (default behavior)
+//
+// https://serverless.com/framework/docs/providers/aws/events/apigateway/#lambda-proxy-integration
+type Response events.APIGatewayProxyResponse
+type Request events.APIGatewayProxyRequest
+
+// Handler is our lambda handler invoked by the `lambda.Start` function call
+func Handler(ctx context.Context, request Request) (Response, error) {
+	body, err := url.ParseQuery(request.Body)
+	scapUrl := ""
+	if err != nil {
+		return Response{StatusCode: 404}, err
+        } else {
+	   scapUrl = body["url"][0]	
+        }
+
+	if len(scapUrl) == 0 {
+		return Response{StatusCode: 404}, nil
+	}
+
+	s, err := goscraper.Scrape(scapUrl, 5)
+	if err != nil {
+	    return Response{StatusCode: 404}, err
+	}
+	var pvw Preview
+	pvw.Url = s.Preview.Link
+	pvw.Title = s.Preview.Title
+	pvw.Description = s.Preview.Description
+	pvw.Images = s.Preview.Images
+	pvw.Image = s.Preview.Images[0]
+	pvw.SiteName = s.Preview.Name
+
+	result, err := json.Marshal(pvw)
+
+	resp := Response{
+		StatusCode:      200,
+		IsBase64Encoded: false,
+		Body:            string(result),
+		Headers: map[string]string{
+			"Content-Type":           "application/json",
+			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Credentials": "true",
+		},
+	}
+
+	return resp, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
